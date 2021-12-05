f85eb9bcda662aa74ac3d17824b03a21ae2e08d8
shanedublin <shanedublin27@gmail.com>
2021-12-04 19:34:09 -0700

    updating app

--- app.js
+++ app.js
@@ -43,26 +43,26 @@ app.get('/api',async function(req, res, next){
     let login = await axios.post(routerUrl + '/login.php',params, config);
     let cookie = login.headers['set-cookie'];
     config.headers.Cookie = cookie;
-
+    
 
     // load the info page to get the login token
     let info = await axios.get(routerUrl + '/info.php?sh=dev',config);
     var root2 = HTMLParser.parse(info.data);
-    
     let js = root2.querySelectorAll('script[type="text/javascript"]');
     let token2Text = js[0].childNodes[0].rawText;
 
-    let split = token2Text.split('var token=')[1];
-    let token2Value = split.replace('\'','');
-    token2Value = token2Value.replace('\'','');
-    token2Value = token2Value.trim();
 
+    let split = token2Text.split('var token=\'')[1];
+    split = split.split('\'');
+    let token2Value = split[0];
+
+    token2Value = token2Value.trim();
     
     // use the 2nd token to pull the json data
     const params2 = new URLSearchParams()
     params2.append('token', token2Value)
     params2.append('do', 'full')
-
+    
     let thatGoodShit = await axios.post(routerUrl + '/info-async.php',params2, config);
     
     res.send(thatGoodShit.data);
